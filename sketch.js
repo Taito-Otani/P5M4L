35bccfdf2e7a6e15a878f4861b2bca11bbe28d49
TaitoOtani <guitar416taito@yahoo.co.jp>
2020-01-25 14:03:01 +0900

    function name  change

--- sketch.js
+++ sketch.js
@@ -1,18 +1,14 @@
-/////
-////
 var s = function(p) {
+        var v1,v2,v3;
 //--------------- Setup ---------------------
     p.setup = function() {
         p.createCanvas(innerWidth, innerHeight);
     
-        window.max.bindInlet('set_value', function(_r, _g, _b) {
-            param1 = _r;
-            param2 = _g;
-            param3 = _b;
-            p.fill(255);
-            p.text(param1, 10, p.height - 20);
-            p.text(param2, 10, p.height - 40);
-            p.text(param3, 10, p.height - 60);
+        window.max.bindInlet('set_value', function(_v1, _v2, _v3) {
+            
+            v1 = _v1;
+            v2 = _v2;
+            v3 = _v3;
     });
   };
 
@@ -23,6 +19,10 @@ var s = function(p) {
         let my = p.height / 2;
         p.rect(0, 0, mx, my);
         window.max.outlet('output', p.frameCount);
+        p.fill(255);
+        p.text(v1, 10, p.height - 20);
+        p.text(v2, 10, p.height - 40);
+        p.text(v3, 10, p.height - 60);
     };
 
     p.mousePressed = function(){
